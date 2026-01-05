09da7ee0119a481750d1998ec39fa1695067774b
kevdru <117737163+kevdru@users.noreply.github.com>
2026-01-05 11:16:22 +0100

    Http -> https

--- LeadDashboard/src/app-data.ts
+++ LeadDashboard/src/app-data.ts
@@ -5,8 +5,8 @@ import { Injectable } from '@angular/core';
 })
 export class AppData {
   private readonly DEV_MODE: boolean = false
-  private readonly BASE_API_URL: string = 'http://leads.imaginecreativeagency.nl/api'
-  private readonly BASE_API_DEV_URL: string = 'http://leads.imaginecreativeagency.nl/api'
+  private readonly BASE_API_URL: string = 'https://leads.imaginecreativeagency.nl/api'
+  private readonly BASE_API_DEV_URL: string = 'https://leads.imaginecreativeagency.nl/api'
     getBaseAPIURL() {
     if(this.DEV_MODE) {
       return this.BASE_API_DEV_URL
