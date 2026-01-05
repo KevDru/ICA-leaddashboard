3ef4df099ffb501094a1b24ee3397045caedb377
Dinan <dinan@example.com>
2026-01-05 12:37:27 +0100

    Outsourced DB creds

--- src/API/db.example.php
+++ src/API/db.example.php
@@ -1,8 +1,8 @@
 <?php
 $host = "localhost";
-$dbname = "ica_leads";
-$user = "admin_leads";
-$pass = "P9jgN#JSEvKc^^mXHCyK";
+$dbname = "leads-dashboard";
+$user = "root";
+$pass = "";
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $pass);
